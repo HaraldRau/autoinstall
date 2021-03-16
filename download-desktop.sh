e024a2ecfcff5cce203c28ee2eb6318033e8dded
Harald Rau <47145786+HaraldRau@users.noreply.github.com>
2021-03-16 09:35:00 +0100

    Update download-desktop.sh

--- download-desktop.sh
+++ download-desktop.sh
@@ -3,5 +3,6 @@ cd /home/schueler/Downloads
 wget https://github.com/HaraldRau/autoinstall/archive/main.zip
 unzip autoinstall-main.zip
 bash /home/schueler/Downloads/autoinstall-main/desktop-reset/desktop-install.sh
+wait
 rm r- autoinstall-main
 rm r- autoinstall-main.zip
